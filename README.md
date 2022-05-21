f87a8a4b82c468ab60bd36e11114ea3593f84950
Jay <jayprakashmandal9@gmail.com>
2022-05-21 14:35:03 +0530

    added readme and requirements files

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# hgs-assignment
+# HGS Assignment
+
+This app is built with python 3
+
+## Instructions to run (without venv)
+Navigate to the project root folder and run
+```bash
+pip install -r requirements.txt
+```
+
+Once above command installs the requirements then run below command to start the app
+
+```bash
+python manage.py runserver
+```
+once this runs successfully, the app can be accessed with the below link
+
+[App link](http://127.0.0.1:8000/)
+
+
+## Instructions to run (with virtual environment)
+Navigate to the project root folder and create a virtual environment using any of your favorite environment package
+
+For example (```virtalenv```):
+```bash
+virtualenv venv
+```
+where ```venv``` is the name of your environment
+
+now it needs to activated
+
+```bash
+source venv/scripts/activate
+```
+once it is activated the name of the environment will be visible on the left
+
+Now continue with these below commands
+```bash
+pip install -r requirements.txt
+```
+
+once above command installs the requirements then run below command to start the app
+
+```bash
+python manage.py runserver
+```
+once this runs successfully, the app can be accessed with the below link
+
+[App link](http://127.0.0.1:8000/)
+
+
+
+#
--- myapp/views.py
+++ myapp/views.py
@@ -15,29 +15,30 @@ def home_page(request):
         if not text or not start or not end:
             messages.add_message(request, messages.ERROR, 'All fields are required')
         # try to convert the start and end values to int
-        try:
-            start = int(start)
-            end = int(end)
+        else:
+            try:
+                start = int(start)
+                end = int(end)
 
-            # start num can't be bigger than end num
-            if start > end:
-                messages.add_message(
-                    request, messages.ERROR,
-                    'Start number can not be bigger than the end number'
-                )
-            # end num can't be bigger than the text length
-            elif end > len(text):
-                messages.add_message(
-                    request, messages.ERROR,
-                    'End number can not be bigger than the text length'
-                )
-            else:
-                colored_text = text[0:start] + "<span style='color:red'>" + text[start:end] + "</span>" + text[end:]
-                context['colored_text'] = colored_text
+                # start num can't be bigger than end num
+                if start > end:
+                    messages.add_message(
+                        request, messages.ERROR,
+                        'Start number can not be bigger than the end number'
+                    )
+                # end num can't be bigger than the text length
+                elif end > len(text):
+                    messages.add_message(
+                        request, messages.ERROR,
+                        'End number can not be bigger than the text length'
+                    )
+                else:
+                    colored_text = text[0:start] + "<span style='color:red'>" + text[start:end] + "</span>" + text[end:]
+                    context['colored_text'] = colored_text
 
-        except ValueError:
-            messages.add_message(request, messages.ERROR,
-                                 'Please enter valid integer values in start and end field')
+            except ValueError:
+                messages.add_message(request, messages.ERROR,
+                                     'Please enter valid integer values in start and end field')
 
         return render(request, 'myapp/home_page.html', context)
 
